f78e606f9e43a57f941ae474bc630d696444b732
Vaish, Mei (UT-EEMCS) <p.vaish@utwente.nl>
2024-11-15 12:14:42 +0100

    add: FastConvolutionalW2Cost.
    compatible with non square grids and produces gradients. optional use of pykeops for larger dimension images.

--- src/training/wasserstein_motion_prior.py
+++ src/training/wasserstein_motion_prior.py
@@ -1,14 +1,15 @@
-''''
-This code is an adaptation of the code at https://gist.github.com/Tenbatsu24/d61cf7a9b19065c8ed6cedf5b4c38ae9
-'''
-
 import math
 
 import torch
 import numpy as np
 
 from einops import rearrange, parse_shape
+from torch.nn.modules.loss import _Loss
 from torch.nn.functional import avg_pool2d, avg_pool3d, interpolate
+
+__all__ = [
+    'FastConvolutionalW2Cost'
+]
 
 SUBSAMPLE = {
     2: (lambda x: 4 * avg_pool2d(x, 2)),
@@ -16,23 +17,39 @@ SUBSAMPLE = {
 }
 
 
+def pyramid(img, dim=2, height=32):
+    img_s = [img]
+    for _ in range(int(math.log2(height))):
+        img = SUBSAMPLE[dim](img)
+        img_s.append(img)
+
+    img_s.reverse()
+    return img_s
+
+
 def epsilon_schedule(p, diameter, blur, scaling):
     r"""Creates a list of values for the temperature "epsilon" across Sinkhorn iterations.
+
     We use an aggressive strategy with an exponential cooling
     schedule: starting from a value of :math:`\text{diameter}^p`,
     the temperature epsilon is divided
     by :math:`\text{scaling}^p` at every iteration until reaching
     a minimum value of :math:`\text{blur}^p`.
+
     Args:
         p (integer or float): The exponent of the Euclidean distance
             :math:`\|x_i-y_j\|` that defines the cost function
             :math:`\text{C}(x_i,y_j) =\tfrac{1}{p} \|x_i-y_j\|^p`.
+
         diameter (float, positive): Upper bound on the largest distance between
             points :math:`x_i` and :math:`y_j`.
+
         blur (float, positive): Target value for the entropic regularization
             (":math:`\varepsilon = \text{blur}^p`").
+
         scaling (float, in (0,1)): Ratio between two successive
             values of the blur scale.
+
     Returns:
         list of float: list of values for the temperature epsilon.
     """
@@ -49,11 +66,14 @@ def epsilon_schedule(p, diameter, blur, scaling):
     return eps_list
 
 
-def batched_convol_bary_debiased(batch, scaling=0.9, weights=None, stab_thresh=1e-30, use_pykeops=False):
+def batched_convol_bary_debiased(
+        batch, scaling=0.9, weights=None, stab_thresh=1e-30, use_pykeops=False, return_cost=False, need_diffable=False
+):
     """
     computes the batched convolutional barycenter2d debiased in the log domain.
     This code is an adpated version of Python Optimal Transport (POT) library and uses ideas from geomloss library.
-    :param batch: input shape (M, B, C, H, W, [D])
+
+    :param batch: input shape (2, B, C, H, W, [D])
     :param weights: the weight for each sample in the bary center calculation
     :param stab_thresh: default 1e-30, for not dividing by zero.
     :return: the batched convolutional barycenter2d debiased images with specified weights
@@ -68,75 +88,197 @@ def batched_convol_bary_debiased(batch, scaling=0.9, weights=None, stab_thresh=1
         raise NotImplementedError("This method is currently not implemented for 3d")
 
     nh = batch.shape[0]  # number of histograms for each image
+
+    assert nh == 2 and return_cost, \
+        "This method is currently implemented for 2 histograms only, one source and one target when return_cost=True"
+
     b = batch.shape[1]  # batch size
 
     if weights is None:
-        weights = torch.ones((nh, 1, 1, 1, 1), dtype=batch.dtype, device=batch.device) / nh
+        weights = torch.zeros((nh, 1, 1, 1, 1), dtype=batch.dtype, device=batch.device)
+        weights[1] = 1  # the weight for the target histogram is one
 
-    log_batch = torch.log(batch + stab_thresh)
-    log_batch = rearrange(log_batch, 'm b c h w -> (m b) c h w')
-    log_img_s = pyramid(log_batch, height=log_batch.shape[-1])[1:]
-    log_img_s = [rearrange(log_img, '(m b) c h w -> m b c h w', m=nh, b=b) for log_img in log_img_s]
+    # check if weights are binary
+    if return_cost and not torch.all(weights[torch.nonzero(weights, as_tuple=True)].eq(1.)):
+        raise ValueError("Weights must be binary for return_cost=True")
 
-    _, b, c, h0, w0 = parse_shape(log_img_s[0], 'm b c h w').values()
-    db = torch.zeros((b, c, h0, w0), dtype=log_batch.dtype, device=log_batch.device)
-    g = torch.zeros(*log_img_s[0].shape, dtype=log_batch.dtype, device=log_batch.device)
+    with torch.no_grad():
+        log_batch = torch.log(batch + stab_thresh)
+        log_batch = rearrange(log_batch, 'm b c h w -> (m b) c h w')
+        log_img_s = pyramid(log_batch, height=min(log_batch.shape[-2:]))[3:]
+        log_img_s = [rearrange(log_img, '(m b) c h w -> m b c h w', m=nh, b=b) for log_img in log_img_s]
 
-    def convol_img(_log_img, _kernel):
-        if use_pykeops:
-            _nh = _log_img.shape[0]  # number of histograms for each image, in some cases this might be one since we update debiasing term db
-            _log_img = LazyTensor(rearrange(_log_img, 'm b c h w -> (m b c w) () h ()'))
-            _log_img = (_log_img + _kernel).logsumexp(dim=2)
-            _log_img = LazyTensor(rearrange(_log_img, '(m b c w) h () -> (m b c h) () w ()', m=_nh, b=b, c=c, w=n))
-            _log_img = (_log_img + _kernel).logsumexp(dim=2)
-            return rearrange(_log_img, '(m b c h) w () -> m b c h w', m=_nh, b=b, c=c, h=n, w=n)
+        _, b, c, h0, w0 = parse_shape(log_img_s[0], 'm b c h w').values()
+        db = torch.zeros((b, c, h0, w0), dtype=log_batch.dtype, device=log_batch.device)
+        g = torch.zeros(*log_img_s[0].shape, dtype=log_batch.dtype, device=log_batch.device,
+                        requires_grad=batch.requires_grad)
+
+        def convol_img(_log_img, _kernel_x, _kernel_y=None):
+            if _kernel_y is None:
+                _kernel_y = _kernel_x
+
+            if use_pykeops:
+                # number of histograms for each image, in some cases this might be one since we update debiasing term db
+                _nh = _log_img.shape[0]
+
+                _log_img = LazyTensor(rearrange(_log_img, 'm b c h w -> (m b c w) () h ()'))
+                _log_img = (_log_img + _kernel_x).logsumexp(dim=2)
+                _log_img = LazyTensor(
+                    rearrange(_log_img, '(m b c w) h () -> (m b c h) () w ()', m=_nh, b=b, c=c, w=w_s))
+                _log_img = (_log_img + _kernel_y).logsumexp(dim=2)
+                return rearrange(_log_img, '(m b c h) w () -> m b c h w', m=_nh, b=b, c=c, h=h_s, w=w_s)
+            else:
+                _log_img = torch.logsumexp(_kernel_x[None, None, None, :, :, None] + _log_img[:, :, :, None, ...],
+                                           dim=-2)
+                _log_img = torch.logsumexp(
+                    _kernel_y[None, None, None, :, :, None] + _log_img[:, :, :, None, ...].permute(0, 1, 2, 3, -1, -2),
+                    dim=-2
+                ).permute(0, 1, 2, -1, -2)
+                return _log_img
+
+        for s, log_img in enumerate(log_img_s):
+            h_s, w_s = log_img.shape[-2:]
+            t_x = torch.linspace(0, 1, w_s, dtype=batch.dtype, device=batch.device)
+            t_y = torch.linspace(0, 1, h_s, dtype=batch.dtype, device=batch.device)
+            if use_pykeops:
+                C_y = - (LazyTensor(t_x.view(1, w_s, 1, 1)) - LazyTensor(t_x.view(1, 1, w_s, 1))) ** 2
+                C_x = - (LazyTensor(t_y.view(1, h_s, 1, 1)) - LazyTensor(t_y.view(1, 1, h_s, 1))) ** 2
+            else:
+                C_y = - (t_x.view(w_s, 1) - (t_x.view(1, w_s))) ** 2
+                C_x = - (t_y.view(h_s, 1) - (t_y.view(1, h_s))) ** 2
+
+            eps_list = epsilon_schedule(2, 2 / max(w_s, h_s), 1 / max(h_s, w_s), scaling)
+            # print(n, len(eps_list), eps_list[0], eps_list[-1])
+
+            for eps in eps_list:
+                m_x = C_x / eps
+                m_y = C_y / eps
+                for _ in range(1 + (s == len(log_img_s) - 1)):  # do the last iteration twice
+                    f = log_img - convol_img(g, m_x, m_y)
+                    log_ku = convol_img(f, m_x, m_y)
+                    log_bar = db + torch.sum(weights * log_ku, dim=0)
+
+                    db = 0.5 * (db + log_bar - convol_img(db[None], m_x, m_y)[0])
+
+                    g = log_bar[None, ...] - log_ku
+
+            # if not the last scale
+            if s != len(log_img_s) - 1:
+                # upscale log_bar, c and g
+                db = interpolate(db, scale_factor=2, mode='bilinear', align_corners=False)
+                g = rearrange(
+                    interpolate(rearrange(g, 'm b c h w -> (m b) c h w'), scale_factor=2, mode='bilinear',
+                                align_corners=False),
+                    '(m b) c h w -> m b c h w', m=nh, b=b
+                )
+                log_bar = interpolate(log_bar, scale_factor=2, mode='bilinear', align_corners=False)
+
+    if need_diffable:
+        with torch.enable_grad():
+            f = torch.log(batch + stab_thresh) - convol_img(g, m_x, m_y)
+            log_ku = convol_img(f, m_x, m_y)
+            log_bar = db + torch.sum(weights * log_ku, dim=0)
+            g = log_bar[None, ...] - log_ku
+
+    if return_cost:
+        dist = batch / batch.sum(dim=(2, 3, 4), keepdim=True)
+        return torch.exp(log_bar), torch.reshape((f[0] * dist[0] + g[0] * dist[1]), (b, -1)).mean(dim=1)
+    else:
+        return torch.exp(log_bar)
+
+
+class FastConvolutionalW2Cost(_Loss):
+    def __init__(self, reduction='mean', scaling=0.9, use_pykeops=False):
+        super(FastConvolutionalW2Cost, self).__init__(reduction=reduction)
+        self.scaling = scaling
+        self.use_pykeops = use_pykeops
+
+    def forward(self, source, target):
+        stacked = torch.stack([source, target], dim=0)
+
+        _, cost = batched_convol_bary_debiased(
+            stacked, scaling=self.scaling, return_cost=True, need_diffable=True, use_pykeops=self.use_pykeops
+        )
+        if self.reduction == 'mean':
+            return cost.mean()
+        elif self.reduction == 'sum':
+            return cost.sum()
         else:
-            _log_img = torch.logsumexp(_kernel[None, None, None, :, :, None] + _log_img[:, :, :, None, ...], dim=-2)
-            _log_img = torch.logsumexp(
-                _kernel[None, None, None, :, :, None] + _log_img[:, :, :, None, ...].permute(0, 1, 2, 3, -1, -2), dim=-2
-            ).permute(0, 1, 2, -1, -2)
-            return _log_img
-
-    for s, log_img in enumerate(log_img_s):
-        n = log_img.shape[-1]
-        t = torch.linspace(0, 1, n, dtype=batch.dtype, device=batch.device)
-        if use_pykeops:
-            C = - (LazyTensor(t.view(1, n, 1, 1)) - LazyTensor(t.view(1, 1, n, 1))) ** 2
-        else:
-            C = - (t.view(n, 1) - (t.view(1, n))) ** 2
-
-        eps_list = epsilon_schedule(2, 2 / n, 1 / n, scaling)
-        # print(n, len(eps_list), eps_list[0], eps_list[-1])
-
-        for eps in eps_list:
-            for _ in range(1 + (s == len(log_img_s) - 1)):  # do the last iteration twice
-                m = C / eps
-                log_ku = convol_img(log_img - convol_img(g, m), m)
-                log_bar = db + torch.sum(weights * log_ku, dim=0)
-
-                db = 0.5 * (db + log_bar - convol_img(db[None], m)[0])
-
-                g = log_bar[None, ...] - log_ku
-
-        # if not the last scale
-        if s != len(log_img_s) - 1:
-            # upscale log_bar, c and g
-            db = interpolate(db, scale_factor=2, mode='bilinear', align_corners=False)
-            g = rearrange(
-                interpolate(rearrange(g, 'm b c h w -> (m b) c h w'), scale_factor=2, mode='bilinear',
-                            align_corners=False),
-                '(m b) c h w -> m b c h w', m=nh, b=b
-            )
-            log_bar = interpolate(log_bar, scale_factor=2, mode='bilinear', align_corners=False)
-
-    return torch.exp(log_bar)
+            return cost
 
 
-def pyramid(img, dim=2, height=32):
-    img_s = [img]
-    for _ in range(int(math.log2(height))):
-        img = SUBSAMPLE[dim](img)
-        img_s.append(img)
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
 
-    img_s.reverse()
-    return img_s
+    # generate an image of a square
+    _image_of_square = torch.zeros(32, 64)
+    # _image_of_square = torch.rand(32, 64)
+    _image_of_square[12:20, 44:52] = 1
+    _image_of_square = _image_of_square.unsqueeze(0)
+
+    # generate an image of a circle
+    _image_of_circle = torch.zeros(32, 64)
+    _y, _x = torch.meshgrid(
+        torch.arange(32, dtype=torch.float32),
+        torch.arange(64, dtype=torch.float32),
+        indexing='ij'
+    )
+    _circle = (_x - 24) ** 2 + (_y - 24) ** 2 <= 3 ** 2
+    _image_of_circle[_circle] = 1
+    _circle = (_x - 8) ** 2 + (_y - 8) ** 2 <= 3 ** 2
+    _image_of_circle[_circle] = 1
+    _image_of_circle = _image_of_circle.unsqueeze(0)
+
+    # show the images
+    _, axes = plt.subplots(1, 2)
+    axes[0].imshow(_image_of_square.cpu().numpy().transpose(1, 2, 0), cmap='gray')
+    axes[1].imshow(_image_of_circle.cpu().numpy().transpose(1, 2, 0), cmap='gray')
+    plt.tight_layout()
+    plt.show()
+
+    # stack the images
+    _source = torch.cat([_image_of_square.unsqueeze(0), _image_of_circle.unsqueeze(0)], dim=0).cuda()
+    _source.requires_grad_(True)
+
+    _loss = FastConvolutionalW2Cost(scaling=0.9, reduction='none')
+
+    _target = _source.roll(1, 0)
+    _cost = _loss(_source, _target)
+
+    [gi_0] = torch.autograd.grad(_cost[0], _source, retain_graph=True)
+    [gi_1] = torch.autograd.grad(_cost[1], _source, retain_graph=False)
+
+    _, ax = plt.subplots(1, 2, figsize=(15, 5))
+    # compute the direction of descent at each point in the gi
+    ax[0].set_title(f'{_cost[0].item():.2f}')
+    ax[0].imshow(gi_0[0].detach().cpu().numpy().transpose(1, 2, 0))
+    ax[0].axis('off')
+    ax[1].set_title(f'{_cost[1].item():.2f}')
+    ax[1].imshow(gi_1[1].detach().cpu().numpy().transpose(1, 2, 0))
+    ax[1].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+    # stack the images
+    _source = torch.cat([_image_of_square.unsqueeze(0), _image_of_circle.unsqueeze(0)], dim=0).cuda()
+    _source.requires_grad_(True)
+
+    _loss = FastConvolutionalW2Cost(scaling=0.9, reduction='mean')
+
+    _target = _source.roll(1, 0)
+    _cost = _loss(_source, _target)
+
+    [gi] = torch.autograd.grad(_cost, _source, retain_graph=False)
+
+    _, ax = plt.subplots(1, 2, figsize=(15, 5))
+    # compute the direction of descent at each point in the gi
+    ax[0].set_title(f'{_cost.item():.2f}')
+    ax[0].imshow(gi_0[0].detach().cpu().numpy().transpose(1, 2, 0))
+    ax[0].axis('off')
+    ax[1].set_title(f'{_cost.item():.2f}')
+    ax[1].imshow(gi_1[1].detach().cpu().numpy().transpose(1, 2, 0))
+    ax[1].axis('off')
+
+    plt.tight_layout()
+    plt.show()
