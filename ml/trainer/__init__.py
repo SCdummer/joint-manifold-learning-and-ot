3c7baedcb2a0aebbb6144a28974136572d10fd7a
Tenbatsu24 <puruvaish24@outlook.com>
2024-11-25 02:02:43 +0100

    add: JointReconODETrainer as part of init

--- ml/trainer/__init__.py
+++ ml/trainer/__init__.py
@@ -1,9 +1,10 @@
-from .base import BaseTrainer, ODETrainer
+from .base import BaseTrainer, ODETrainer, JointReconODETrainer
 from .util import MyWandBLogger, get_best_ckpt
 
 __all__ = [
     'BaseTrainer',
     'ODETrainer',
+    'JointReconODETrainer',
     'MyWandBLogger',
     'get_best_ckpt'
 ]
