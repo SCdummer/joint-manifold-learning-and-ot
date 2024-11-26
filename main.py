cb5f2066506779d1b28c82fe1ae73acdd483e5e8
Tenbatsu24 <puruvaish24@outlook.com>
2024-11-26 22:49:37 +0100

    change: use the custom collate function of the HeLa cells dataset and use weighted sampler since successive frames introduce duplications of data in training that might cause overfitting issues.

--- main.py
+++ main.py
@@ -58,14 +58,15 @@ def train_recon(cfg, fast_dev_run=False):
         DataLoader(
             dataset=ds, batch_size=cfg.batch_size, num_workers=num_w, shuffle=shuffle,
             pin_memory=True, persistent_workers=True, drop_last=skip_last,
-            generator=torch.Generator().manual_seed(cfg.training.dataloader_seed) if shuffle else None
+            generator=torch.Generator().manual_seed(cfg.training.dataloader_seed) if shuffle else None,
+            collate_fn=dataset_class.get_collate_fn()
         )
         for ds, num_w, shuffle, skip_last in [(train_ds, cfg.num_workers, True, True), (val_ds, 1, False, False)]
     ]
 
     test_dl = DataLoader(
         dataset=test_ds, batch_size=cfg.batch_size, num_workers=1, shuffle=False,
-        pin_memory=True, persistent_workers=True, drop_last=False,
+        pin_memory=True, persistent_workers=True, drop_last=False, collate_fn=dataset_class.get_collate_fn()
     ) if test_ds else None
 
     # handle varying batch sizes and training steps so that it does not have to be hardcoded
@@ -107,7 +108,7 @@ def train_recon(cfg, fast_dev_run=False):
     return ae
 
 
-def train_ode(cfg, ae, fast_dev_run=False):
+def train_ode(cfg, fast_dev_run=False):
     logger = MyWandBLogger(
         name=cfg.run_name, project=cfg.project, log_model='all', id=cfg.run_id, allow_val_change=True
     )
@@ -149,18 +150,27 @@ def train_ode(cfg, ae, fast_dev_run=False):
     else:
         test_ds = None
 
+    t_sampler = torch.utils.data.WeightedRandomSampler(
+        train_ds.get_sampling_weights(), 500, replacement=True
+    )
+    v_sampler = torch.utils.data.WeightedRandomSampler(
+        val_ds.get_sampling_weights(), 16 * cfg.batch_size, replacement=True
+    )
     t_dl, v_dl = [
         DataLoader(
             dataset=ds, batch_size=cfg.batch_size, num_workers=num_w, shuffle=shuffle,
             pin_memory=True, persistent_workers=True, drop_last=skip_last,
-            generator=torch.Generator().manual_seed(cfg.training.dataloader_seed) if shuffle else None
+            generator=torch.Generator().manual_seed(cfg.training.dataloader_seed) if shuffle else None,
+            sampler=sampler, collate_fn=ds.get_collate_fn()
         )
-        for ds, num_w, shuffle, skip_last in [(train_ds, cfg.num_workers, True, True), (val_ds, 1, False, False)]
+        for ds, num_w, shuffle, skip_last, sampler in [
+            (train_ds, cfg.num_workers, False, True, t_sampler), (val_ds, 1, False, False, v_sampler)
+        ]
     ]
 
     test_dl = DataLoader(
         dataset=test_ds, batch_size=cfg.batch_size, num_workers=1, shuffle=False,
-        pin_memory=True, persistent_workers=True, drop_last=False,
+        pin_memory=True, persistent_workers=True, drop_last=False, collate_fn=dataset_class.get_collate_fn()
     ) if test_ds else None
 
     # handle varying batch sizes and training steps
@@ -177,10 +187,11 @@ def train_ode(cfg, ae, fast_dev_run=False):
     metrics = {m.short_name: getattr(torchmetrics, m.name)(**m.params) for m in cfg.metrics}
 
     ode_training_module_cls = JointReconODETrainer
+    ae = REGISTRY['model'].get('HeLa-ae')()
     neural_ode = REGISTRY['model'].get(cfg.model.name)(**cfg.model.params)
     training_module = ode_training_module_cls(
         cfg, neural_ode, loss_fn, metrics, dataset_class.NORMALIZE, dataset_class.DE_NORMALIZE,
-        ae.encoder, ae.decoder
+        ae.encoder, ae.decoder, n=cfg.dataset.train.n_successive + 1
     )
 
     trainer.fit(
@@ -203,77 +214,21 @@ if __name__ == '__main__':
     import os
     from ml_collections import ConfigDict
 
-    os.environ['WANDB_MODE'] = 'disabled'
+    os.environ['WANDB_MODE'] = 'offline'
     # os.environ["HTTPS_PROXY"] = 'http://proxy.utwente.nl:3128'
 
     split_seed = int.from_bytes(os.urandom(4), 'little')
     lightning_seed = int.from_bytes(os.urandom(4), 'little')
     dataloader_seed = int.from_bytes(os.urandom(4), 'little')
 
-    for (_dataset, epochs) in [('HeLa', 100)]:
+    for (_dataset, epochs) in [('HeLa', 200)]:
+        n = 3
         num_channels = 1 if _dataset in ['HeLa'] else 3
-        b_s = 64
+        b_s = 64 // (n + 1)
         wd_recon = 1e-4
-        wd_ode = 1e-6
+        wd_ode = 0
 
         run_name = _dataset
-
-        _cfg = {
-            'seed': lightning_seed,
-            'run_name': f'{run_name}_recon',
-            'project': 'HeLa',
-            'run_id': None,
-            'training': {
-                'metric_to_monitor': 'val/loss', 'metric_mode': 'min',
-                'save_top_k': 1, 'save_last': True,
-                'accelerator': 'gpu', 'precision': '32-true',
-                'devices': 1, 'accumulate_grad_batches': 1, 'strategy': 'auto',
-                'num_sanity_val_steps': 0,
-                'gradient_clip_val': 5,
-                'benchmark': True,
-                'max_epochs': epochs,
-                'ckpt_path': None,
-                'dataloader_seed': dataloader_seed,
-            },
-            'dataset': {
-                'name': _dataset,
-                'train': {'split': 'train', 'seed': split_seed},
-                'val': {'split': 'val', 'seed': split_seed},
-            },
-            'data_dir': os.path.join('.', 'data'),
-            'batch_size': b_s,
-            'num_workers': 2,
-            'model': {
-                'name': f'{_dataset}-ae',
-                'params': {}
-            },
-            'loss_fn': {
-                'name': 'MSELoss', 'params': {}
-            },
-            'metrics': [
-                ConfigDict({
-                    'name': 'MeanSquaredError', 'short_name': 'mse',
-                    'params': {'squared': True}
-                }),
-            ],
-            'opt': {
-                'turn_off_norm_weight_decay': True,
-                'name': 'SGD', 'params': {'lr': 0.1, 'momentum': 0.9, 'weight_decay': wd_recon, 'nesterov': True},
-            },
-            'lr_scheduler': {
-                'name': ['get_cosine_schedule_with_warmup', ],
-                'params': [
-                    {
-                        'num_warmup_steps': 0, 'num_training_steps': epochs,
-                        'lr_min': 1e-6, 'start_factor': 1e-6,
-                    }
-                ],
-                'interval': ['step', ],
-            }
-        }
-        _cfg = ConfigDict(_cfg, convert_dict=True)
-        ae = train_recon(_cfg, fast_dev_run=False)
-        wandb.finish()
 
         _cfg = {
             'seed': lightning_seed,
@@ -294,29 +249,29 @@ if __name__ == '__main__':
             },
             'dataset': {
                 'name': _dataset,
-                'train': {'split': 'train', 'seed': split_seed},
-                'val': {'split': 'val', 'seed': split_seed},
+                'train': {'split': 'train', 'seed': split_seed, 'n_successive': n},
+                'val': {'split': 'val', 'seed': split_seed, 'n_successive': n},
             },
             'data_dir': os.path.join('.', 'data'),
             'batch_size': b_s,
             'num_workers': 2,
             'model': {
                 'name': f'{_dataset}-ode',
-                'params': {'dim': 512, 'timesteps': 1}
+                'params': {'dim': 512, 'timesteps': 10}
             },
             'loss_fn': {
                 'name': 'MSELoss', 'params': {}
             },
             'metrics': [
-                ConfigDict({
-                    'name': 'MeanSquaredError', 'short_name': 'mse',
-                    'params': {'squared': True}
-                }),
+                # ConfigDict({
+                #     'name': 'MeanSquaredError', 'short_name': 'mse',
+                #     'params': {'squared': True}
+                # }),
             ],
             'opt': {
                 'turn_off_norm_weight_decay': True,
-                'name': 'AdamW',
-                'params': {'lr': 1e-3, 'betas': (0.9, 0.999), 'eps': 1e-8, 'weight_decay': wd_ode, 'amsgrad': False},
+                'name': 'SGD', 'params': {'lr': 0.1, 'momentum': 0.9, 'weight_decay': 1e-4, 'nesterov': True},
+                # 'name': 'Adam', 'params': {'lr': 5e-4, 'weight_decay': wd_ode},
             },
             'lr_scheduler': {
                 'name': ['get_cosine_schedule_with_warmup', ],
@@ -330,5 +285,5 @@ if __name__ == '__main__':
             }
         }
         _cfg = ConfigDict(_cfg, convert_dict=True)
-        train_ode(_cfg, ae, fast_dev_run=False)
+        train_ode(_cfg, fast_dev_run=False)
         wandb.finish()
