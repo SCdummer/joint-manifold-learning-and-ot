d2c15c2e08b176e8a3ed31c0669d9fc0f9897e1b
Tenbatsu24 <puruvaish24@outlook.com>
2024-11-27 13:39:42 +0100

    add: option to get regularier from optim

--- ml/optim/__init__.py
+++ ml/optim/__init__.py
@@ -1,7 +1,9 @@
 from .loss import get_loss
+from .regulariser import get_regulariser
 from .optim import init_optims_from_config
 
 __all__ = [
     'init_optims_from_config',
-    'get_loss'
+    'get_loss',
+    'get_regulariser'
 ]
