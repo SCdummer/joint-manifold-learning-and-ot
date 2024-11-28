4d925f431cc13dcecb229de6a6e3cb55e30f6668
Tenbatsu24 <puruvaish24@outlook.com>
2024-11-28 13:22:45 +0100

    change: dataset for n successive made compatible with train.py

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -1,121 +1,84 @@
-import os
 import re
+from pathlib import Path
 
 import torch
 import numpy as np
+import torchvision.transforms as T
+
 from PIL import Image
+from sklearn.model_selection import train_test_split
+from torchvision.datasets import VisionDataset
 
 
-class CellData(torch.utils.data.Dataset):
-    def __init__(self, data_dir, time_step=10, dynamic=True, full_time_series=False):
-        super(CellData, self).__init__()
+class BaseDataset(VisionDataset):
+    IMG_SIZE = (64, 64)
 
-        # Save the inputs
-        self.data_dir = data_dir
-        self.time_step = time_step
-        self.dynamic = dynamic
+    DEFAULT_TRANSFORM = T.Compose([
+        T.ToTensor(),
+    ])
+
+    BASE_FOLDER = 'Fluo-N2DL-HeLa'
+    TRACKS_FOLDER = '01_processed'
+
+    EXTENSION = 'tif'
+
+    def __init__(self, root, split='train', seed=None, test_size=0.2, transform=None, full_time_series=False):
+        root = Path(root, self.BASE_FOLDER, self.TRACKS_FOLDER)
+        super(BaseDataset, self).__init__(root)
+
+        # get all the tracks, these are folders in the root_dir
+        tracks = [d for d in root.iterdir() if d.is_dir() and re.match(r'Track\d+', d.name)]
+        print(f'Found {len(tracks)} tracks')
+        # print the minimum and maximum number of images in a track
+        print(
+            'Min number of images:', min(
+                len(list(d.glob(f'*.{self.EXTENSION}'))) for d in tracks
+            )
+        )
+        print(
+            'Max number of images:', max(
+                len(list(d.glob(f'*.{self.EXTENSION}'))) for d in tracks
+            )
+        )
+
+        if seed is not None:
+            train_tracks, test_tracks = train_test_split(tracks, random_state=seed, test_size=test_size)
+            tracks = train_tracks if split == 'train' else test_tracks
+
+        self.all_images_per_track = {
+            track: list(sorted(list(track.glob(f'*.{self.EXTENSION}'))))
+            for track in tracks
+        }
+        self.all_images_per_track = {
+            track: (
+                images,
+                int(re.match(rf'track(\d+)_t(\d+).{self.EXTENSION}', images[0].name).group(2)),
+                int(re.match(rf'track(\d+)_t(\d+).{self.EXTENSION}', images[-1].name).group(2))
+            )
+            for track, images in self.all_images_per_track.items()
+        }
+        self.track_weights = {
+            track: 1 / len(images) for track, (images, _, _) in self.all_images_per_track.items()
+        }
+
+        # we need to sample pairs of images that are n consecutive in time
+        # thereofore, if n=0, it is only the image itself, n=1 is the image and the next one, etc.
+        self.image_tracks = []
+        self.sample_weights = []
+        for track, (images, start_time, end_time) in self.all_images_per_track.items():
+            for i in range(len(images) - 1):
+                self.image_tracks.append((images[i], images[i + 1]))
+                self.sample_weights.append(self.track_weights[track])
+
+        if transform is None:
+            transform = self.DEFAULT_TRANSFORM
+
+        self.transform = transform
+        self.split = split
         self.full_time_series = full_time_series
 
-        # Define a dictionary that will contain the data
-        self.data_dict = {}
-
-        # Save the maximum value of the images and the minimum value. This is used for normalization purposes.
-        self.max_val = -100000000000000
-        self.min_val = 100000000000000
-
-        # Create a dictionary that saves all the image filenames per track and not only a subsampling.
-        self.all_images_per_track = {}
-
-        # Load all the data in a dictionary
-        for folder in os.listdir(data_dir):
-
-            # Get the track id
-            track_id = int(re.search(r'\d+$', folder).group())
-
-            # Initialize the entry corresponding to this id
-            self.data_dict[track_id] = []
-
-            # Grab all the images
-            img_filenames = sorted(os.listdir(os.path.join(data_dir, folder)))
-
-            # Get the beginning and end time
-            begin_time = int(img_filenames[0][-7:-4])
-            end_time = int(img_filenames[-1][-7:-4])
-
-            # Load the images and add them to the dictionary
-            for filename in img_filenames:
-
-                # Get the time
-                time = int(filename[-7:-4])
-
-                # Load the image
-                img = np.array(Image.open(os.path.join(data_dir, folder, filename)))
-                if len(img.shape) == 2:
-                    img = img[np.newaxis, :, :]
-
-                # In case we want to save all the images
-                if track_id not in self.all_images_per_track:
-                    self.all_images_per_track[track_id] = ([os.path.join(data_dir, folder, filename)], begin_time, end_time)
-                else:
-                    self.all_images_per_track[track_id][0].append(os.path.join(data_dir, folder, filename))
-
-                # Skip this one if it does not satisfy the time step
-                if time % time_step == 0:
-
-                    # In this case save the data in the dictionary of images that we want to use.
-                    self.data_dict[track_id].append(img)
-
-                    # Update the maximum and minimum values
-                    self.max_val = max(self.max_val, img.max())
-                    self.min_val = min(self.min_val, img.min())
-
-                else:
-                    continue
-
-        # We need a mapping from an index to two consecutive time points as the latter will be sampled. We also have
-        # the same but then for the individual images
-        idx_to_track_id_time_pair = {}
-        idx_to_track_id_image = {}
-        idx_counter_dynamic = 0
-        idx_counter_static = 0
-        for track_id, img_list in self.data_dict.items():
-
-            # Get the number of time points
-            num_time_points = len(img_list)
-
-            # For every image pair, do ...
-            for pair_idx in range(num_time_points-1):
-                idx_to_track_id_time_pair[idx_counter_dynamic] = (track_id, pair_idx, pair_idx+1)
-                idx_counter_dynamic += 1
-
-            # For every image, do ...
-            for img_idx in range(num_time_points):
-                idx_to_track_id_image[idx_counter_static] = (track_id, img_idx)
-                idx_counter_static += 1
-
-        # Save the dictionaries as attributes
-        self.idx_to_track_id_time_pair = idx_to_track_id_time_pair
-        self.idx_to_track_id_image = idx_to_track_id_image
-
-        # Save the number of image pairs and the number of images
-        self.num_img_pairs = idx_counter_dynamic - 1
-        self.num_imgs = idx_counter_static - 1
-
-        # Get the tracks that you have
-        self.track_ids = list(self.data_dict.keys())
-
-        # Save the scaling factor induced by the calculated maximum and minimal values. This scales all images to
-        # the interval [0, 1].
-        self.scaling_factor = (1.0 / (self.max_val - self.min_val)).astype(np.float32)
-
-    def __len__(self):
-        if self.full_time_series:
-            return len(self.track_ids)
-        else:
-            if self.dynamic:
-                return self.num_img_pairs
-            else:
-                return self.num_imgs
+    def get_sampling_weights(self):
+        return self.sample_weights
 
     def get_full_track(self, track_idx):
         if isinstance(track_idx, str):
@@ -124,19 +87,143 @@ class CellData(torch.utils.data.Dataset):
             track_images = self.all_images_per_track[list(self.all_images_per_track.keys())[track_idx]][0]
         else:
             raise ValueError('track_idx must be either a string or an integer')
-        np_array_track = list(map(lambda x: np.array(Image.open(x)), track_images))
-        return [img[np.newaxis, :, :] if len(img.shape) == 2 else img for img in np_array_track]
+        return list(
+            map(lambda x: self.DEFAULT_TRANSFORM(np.array(Image.open(x), dtype=np.float32)[..., None]), track_images)
+        )
 
+    def get_tracks_selected(self):
+        return list(self.all_images_per_track.keys())
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, idx):
+        raise NotImplementedError
+
+
+class HeLaCellsSuccessive(BaseDataset):
+
+    def __init__(
+            self, root, n_successive=0, subsampling=1, full_time_series=False,
+            split='train', seed=None, test_size=0.2, transform=None
+    ):
+        super(HeLaCellsSuccessive, self).__init__(root, split, seed, test_size, transform, full_time_series)
+        self.n_successive = n_successive
+        self.subsampling = subsampling
+
+        self.image_tracks = {}
+        self.sample_weights = []
+        mult = self.subsampling
+        for track_id, (track, (images, start_time, end_time)) in enumerate(self.all_images_per_track.items()):
+            self.image_tracks[track_id] = []
+            for i in range(0, len(images) - n_successive, mult):
+                if i + mult * n_successive + 1 > len(images):
+                    break
+
+                self.image_tracks[track_id].append(
+                    (
+                        images[i:i + mult * n_successive + 1:mult],
+                        ((start_time + i) / end_time, (start_time + i + n_successive * mult) / end_time)
+                    )
+                )
+                self.sample_weights.append(self.track_weights[track])
+
+        self.flat_image_tracks = sum(self.image_tracks.values(), [])
+
+    @staticmethod
+    def get_collate_fn():
+        # stack the images along the batch dimension using concatenation
+        def collate_fn(batch):
+            # batch is a list of tuples (x, t) where x is a list of images [(c, h, w) * n] and t is the time (n,)
+            # first chunk should be the images[0] of each track, second chunk should be images[1] of each track etc.
+            x, t = zip(*batch)  # x is a list of [(c, h, w) * n] and t is a list of (n,)
+            x, t = list(zip(*x)), list(zip(*t))
+            x, t = [torch.stack(x_i, dim=0) for x_i in x], [torch.stack(t_i, dim=0) for t_i in t]
+            x, t = torch.concat(x, dim=0), torch.concat(t, dim=0)
+            return x, t
+
+        return collate_fn
+
+    def get_sampling_weights(self):
+        return self.sample_weights
+
+    def __len__(self):
+        if self.full_time_series:
+            # if we are getting full time series then we just get the tracks in train / val set and return all images
+            # with no subsampling
+            return len(self.all_images_per_track)
+        else:
+            # else we return the number of image tracks after subsampling and taking into account the number of
+            # successive images
+            return len(self.flat_image_tracks)
 
     def __getitem__(self, idx):
         if self.full_time_series:
-            return (np.stack(self.data_dict[self.track_ids[idx]], axis=0) - self.min_val) * self.scaling_factor
+            xs, start_time, end_time = self.all_images_per_track[list(self.all_images_per_track.keys())[idx]]
+            t = torch.linspace(start_time / end_time, 1, steps=len(xs), dtype=torch.float32)
         else:
-            if self.dynamic:
-                track_id, img_idx_1, img_idx_2 = self.idx_to_track_id_time_pair[idx]
-                data = tuple([(self.data_dict[track_id][i] - self.min_val) * self.scaling_factor for i in range(img_idx_1, img_idx_2+1)])
-                return (track_id, img_idx_1, img_idx_2, data)
-            else:
-                track_id, img_idx = self.idx_to_track_id_image[idx]
-                return (track_id, img_idx, (self.data_dict[track_id][img_idx] - self.min_val) * self.scaling_factor)
+            xs, times = self.flat_image_tracks[idx]
+            t = torch.linspace(times[0], times[1], steps=self.n_successive + 1, dtype=torch.float32)
+        xs = [np.array(Image.open(x), dtype=np.float32)[..., None] for x in xs]
+        xs = [self.transform(x) for x in xs]
+        return xs, t
 
+
+class SquaresMovingHorizontallySuccessive(HeLaCellsSuccessive):
+    BASE_FOLDER = 'Squares_horizontally_moving'
+    TRACKS_FOLDER = 'images'
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    _n_successive = 1
+    _ds = HeLaCellsSuccessive(
+        Path('..', '..', 'data'), n_successive=_n_successive, subsampling=5,
+        split='test', test_size=0.2, seed=None, full_time_series=True
+    )
+
+    print(_ds.get_tracks_selected())
+    print(len(_ds.get_full_track(0)))
+    print(_ds.get_full_track(0)[0].shape)
+
+    _dl = iter(
+        torch.utils.data.DataLoader(
+            _ds, batch_size=64 // (_n_successive + 1), shuffle=True,
+            # sampler=torch.utils.data.WeightedRandomSampler(
+            #     _ds.get_sampling_weights(), 200, replacement=True
+            # ),
+            collate_fn=_ds.get_collate_fn()
+        )
+    )
+
+    print(repr(_ds))
+
+    mean, std = 0, 0
+    for _i in range(5):
+        _x, _t = next(_dl)
+        mean += torch.mean(_x).item()
+        std += torch.std(_x).item()
+
+        print(_x.shape, _t.shape)
+
+        skipper = _x.size(0) // (_n_successive + 1)
+
+        _x = _x[::skipper]
+        _t = _t[::skipper]
+
+        print(_x.shape, _t.shape)
+
+        _, _ax = plt.subplots(1, _n_successive + 1, figsize=(4 * (_n_successive + 1), 4))
+        if _n_successive == 0:
+            _ax = [_ax]
+        for _i, (_x_i, _t_i) in enumerate(zip(_x, _t)):
+            print(f'Min: {torch.min(_x_i[0])}, Max: {torch.max(_x_i[0])}')
+            _normed_xi = (_x_i[0] - torch.min(_x_i[0])) / (torch.max(_x_i[0]) - torch.min(_x_i[0]))
+            _ax[_i].imshow(_normed_xi, cmap='gray')
+            _ax[_i].set_title(f'{_t_i:.2f}')
+            _ax[_i].axis('off')
+        plt.show()
+        plt.close()
+
+    print(mean / len(_dl), std / len(_dl))
